3f66284d981060d6bca0c3607fb6b45f3de63977
gmachin-stratio <gmachin@stratio.com>
2023-11-22 13:27:55 +0100

    Modify 04-paquetes.py

--- 04-paquetes.py
+++ 04-paquetes.py
@@ -12,3 +12,9 @@ from paquete_1.subpaquete_1 import sumar
 
 longitud.longitud(1, 2, 3, 4)
 sumar.sumar(1, 2, 3, 4)
+
+
+import paquete_1 as pck
+
+pck.longitud.longitud(1, 2, 3, 4)
+pck.subpaquete_1.sumar.sumar(1, 2, 3, 4)
